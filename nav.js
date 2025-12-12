bf4ebd77e8370611a2715cee16a8a26abc6c2602
AGrumpySoul <155701008+SoulTheOwlGod@users.noreply.github.com>
2025-12-12 13:00:03 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -5,6 +5,8 @@ const navOrb = document.getElementById('navOrb');
 function openModal() { modalBg.style.display = 'flex'; }
 function closeModal() { modalBg.style.display = 'none'; }
 
+navOrb.addEventListener('click', openModal);
+
 modalBg.addEventListener('click', e => {
   if(e.target === modalBg) closeModal();
 });
